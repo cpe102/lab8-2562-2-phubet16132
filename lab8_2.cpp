385317e508fb255fd4d6d195672d31b9539e2e23
phubet16132 <59276795+phubet16132@users.noreply.github.com>
2020-01-24 15:46:19 +0700

    Update lab8_2.cpp

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -2,7 +2,23 @@
 using namespace std;
 
 //Write the function printO() here
-
+void printO(int N,int M){
+	int in=1,im=1;
+	if(N<=0 or M<=0){
+		cout<<"Invalid input";
+	}
+	while(in<=N){
+		while(im<=M){
+			cout<<"o";
+			im+=1;
+		}
+		cout<<endl;
+		in+=1;
+		im=1;
+	}
+	
+}
+		
 int main(){
 	
 	printO(2,2);
